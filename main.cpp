621dfc7bed5d3d2c5baab13e688ba60a86e62690
WenchangGaoT <106501119+WenchangGaoT@users.noreply.github.com>
2024-10-27 16:18:58 -0400

    debugged

--- main.cpp
+++ main.cpp
@@ -31,36 +31,4 @@ int main(int argc, char *argv[]) {
     char path[] = "image.ppm";
     Canvas can(13, spheres, camera, path);
     can.render(samps, w, h, d, 0.5135);
-
-
-//     int w=1024, h=768, samps = (argc == 2 ? atoi(argv[1]) / 4 : 1);
-//     Line3 cam(Vec3(50,52,295.6), Vec3(0,-0.042612,-1).normal()); // cam pos, dir
-//     char path[] = "image.ppm";
-//     Canvas can(8, spheres, cam, path);
-//     Vec3 cx=Vec3(w*.5135 / h), cy=((cx.det(can.camera.d)).normal())*.5135, r, *c=new Vec3[w * h];
-//     Line3  rr;
-    
-// #pragma omp parallel for schedule(dynamic, 1) private(r)       // OpenMP
-//     for (unsigned short y = 0; y < h; y++){                       // Loop over image rows
-//         unsigned short oo = y * y * y, Xi[3] = {0, 0, oo};
-//         fprintf(stderr,"\rRendering (%d spp) %5.2f%%", samps * 4, 100.* y/(h-1));
-//         for (unsigned short x = 0; x < w; x++)   // Loop cols
-//             for (int sy = 0, i = (h-y-1) * w + x; sy < 2; sy++)     // 2x2 subpixel rows
-//                 for (int sx = 0; sx < 2; sx++, r = Vec3()){        // 2x2 subpixel cols
-//                     for (int s = 0; s < samps; s++){
-//                         double r1 = 2 * rnd::erand48(Xi), dx = r1<1 ? sqrt(r1) - 1 : 1 - sqrt(2 - r1);
-//                         double r2 = 2 * rnd::erand48(Xi), dy = r2<1 ? sqrt(r2) - 1 : 1 - sqrt(2 - r2);
-//                         // double r1 = 2 * rnd::erand48(Xi), dx = r1 - 1;
-//                         // double r2 = 2 * rnd::erand48(Xi), dy = r2 - 1;
-//                         Vec3 d = cx*( ( (sx+.5 + dx)/2 + x) / w - .5) +
-//                                 cy*( ( (sy+.5 + dy)/2 + y) / h - .5) + cam.d;
-//                         r = r + can.radiance(Line3(cam.o+d * 140,d.normal()),0,Xi)*(1./samps);
-//                     } // Camera rays are pushed ^^^^^ forward to start in interior
-//                     c[i] = c[i] + Vec3(clamp(r.x),clamp(r.y),clamp(r.z)) *.25;
-//                 }
-//     }
-//     FILE *f = fopen("image.ppm", "w");         // Write image to PPM file.
-//     fprintf(f, "P3\n%d %d\n%d\n", w, h, 255);
-//     for (int i=0; i<w*h; i++)
-//         fprintf(f,"%d %d %d ", toInt(c[i].x), toInt(c[i].y), toInt(c[i].z));
 }
